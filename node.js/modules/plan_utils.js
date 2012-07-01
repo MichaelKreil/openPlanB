0062e1c125738f20e650e121957c21e10e7eb07c
Micho <github@michael-kreil.de>
2012-07-01 19:57:02 +0200

    planu

--- node.js/modules/plan_utils.js
+++ node.js/modules/plan_utils.js
@@ -51,6 +51,7 @@ function decodeFile(file, outputFolder) {
 		case 'plankant': decodePlanKANT( file.fullname, outputFile); break;
 		case 'plankgeo': decodePlanKGEO( file.fullname, outputFile); break;
 		case 'planlauf': decodePlanLAUF( file.fullname, outputFile); break;
+		case 'planu':    decodePlanU(    file.fullname, outputFile); break;
 		case 'planw':    decodePlanW(    file.fullname, outputFile); break;
 		case 'planzug':  decodePlanZUG(  file.fullname, outputFile); break;
 		default:
@@ -418,6 +419,68 @@ function decodePlanLAUF(filename, outputFile) {
 	
 	exportHeader(outputFile, header);
 	exportTSV(outputFile, '1', data1);
+}
+
+function decodePlanU(filename, outputFile) {
+	var header = {unknown:[]};
+	
+	var f = new PlanFile(filename);
+	
+	header.size = f.readInteger(2);
+	header.version = f.readInteger(2) + '.' + f.readInteger(2);
+	header.creationDate = f.readTimestamp();
+	
+	header.unknown.push(f.readInteger(2));
+	header.unknown.push(f.readInteger(2));
+	header.listLength1 = f.readInteger(4);
+	header.unknown.push(f.readInteger(4));
+	header.listLength2 = f.readInteger(4);
+	
+	f.checkBytes('00 00 00 00');
+	f.checkBytes('00 00 00 00');
+	f.checkBytes('00 00 00 00');
+	f.checkBytes('00 00 00 00');
+	
+	header.unknown.push(f.readInteger(4));
+	header.unknown.push(f.readInteger(4));
+	
+	header.unknown.push(f.getHexDump(4));
+	
+	f.checkBytes('00 00 00 00');
+	
+	header.unknown.push(f.readInteger(2));
+	header.unknown.push(f.readInteger(2));
+	
+	header.description = f.readString(header.size - f.pos);
+	
+	var
+		data1 = [],
+		data2 = [];
+	
+	for (var i = 0; i < header.listLength1; i++) {
+		data1[i] = [];
+		data1[i][0] = f.readInteger(4);
+		data1[i][1] = f.readInteger(2);
+		data1[i][2] = f.readInteger(2);
+	}
+	for (var i = 0; i < header.listLength2; i++) {
+		data2[i] = [];
+		data2[i][0] = f.readInteger(4);
+		data2[i][1] = f.readInteger(4);
+		data2[i][2] = f.readInteger(2);
+		data2[i][3] = f.readInteger(4);
+		data2[i][4] = f.readInteger(2);
+		data2[i][5] = f.readInteger(1);
+		data2[i][6] = f.readInteger(1);
+	}
+	
+	f.checkBytes('FF FF FF FF FF FF FF FF FF FF FF FF FF FF FF FF FF FF');
+	
+	header.bytesLeft = f.check(outputFile);
+	
+	exportHeader(outputFile, header);
+	exportTSV(outputFile, '1', data1);
+	exportTSV(outputFile, '2', data2);
 }
 
 function decodePlanW(filename, outputFile) {
