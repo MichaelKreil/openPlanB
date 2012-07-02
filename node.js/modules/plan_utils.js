287b535b908b375f737160271c8fb36265d8099e
Micho <github@michael-kreil.de>
2012-07-03 01:13:46 +0200

    plankgeo finished

--- node.js/modules/plan_utils.js
+++ node.js/modules/plan_utils.js
@@ -520,9 +520,11 @@ function decodePlanKANT(filename, outputFile) {
 }
 
 function decodePlanKGEO(filename, outputFile) {
-	var header = {unknown:[]};
-	
 	var f = new PlanFile(filename);
+	
+	// Header
+	
+	var header = {unknown:[]};
 	
 	header.size = f.readInteger(2);
 	header.version = f.readInteger(2) + '.' + f.readInteger(2);
@@ -538,21 +540,38 @@ function decodePlanKGEO(filename, outputFile) {
 	
 	header.description = f.readString(header.size - f.pos);
 	
-	var data1 = [];
+	// Data
+	
+	var list1 = [];
 	
 	for (var i = 0; i < header.listLength1; i++) {
-		data1[i] = [];
-		data1[i][0] = f.readInteger(-2);
+		list1[i] = [];
+		list1[i][0] = f.readInteger(-2);
 	}
 	
-	for (var i = 0; i < header.listLength1; i++)	data1[i][1] = f.readInteger(-2);
-	for (var i = 0; i < header.listLength1; i++)	data1[i][2] = f.readInteger(-2);
-	for (var i = 0; i < header.listLength1; i++)	data1[i][3] = f.readInteger(-2);
+	for (var i = 0; i < header.listLength1; i++)	list1[i][1] = f.readInteger(-2);
+	for (var i = 0; i < header.listLength1; i++)	list1[i][2] = f.readInteger(-2);
+	for (var i = 0; i < header.listLength1; i++)	list1[i][3] = f.readInteger(-2);
 	
 	header.bytesLeft = f.check(outputFile);
 	
+	// Structure
+	
+	var data = [];
+	
+	for (var i = 0; i < header.listLength1; i++) {
+		data[i] = {
+			id: i,
+			lon: (list1[i][0] + list1[i][2]/1000) / (40048/360),   // Der HAFAS-Ellipsoid ist eine Kugel mit einem Umfang von 40048km
+			lat: (list1[i][1] + list1[i][3]/1000) / (40048/360)
+		}
+	}
+	
+	// Export
+	
 	exportHeader(outputFile, header);
-	exportTSV(outputFile, '1', data1);
+	exportTSV(outputFile, '1', list1);
+	exportJSON(outputFile, 'data', data);
 }
 
 function decodePlanLAUF(filename, outputFile) {
