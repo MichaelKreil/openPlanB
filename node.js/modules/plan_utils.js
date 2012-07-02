080b4ac23cc91762f59a763a0bef3eb5f383fc8f
Micho <github@michael-kreil.de>
2012-07-02 21:22:44 +0200

    planb finished part 2

--- node.js/modules/plan_utils.js
+++ node.js/modules/plan_utils.js
@@ -77,9 +77,13 @@ exports.getAllPlanFiles = getAllPlanFiles;
 
 
 function decodePlanB(filename, outputFile) {
-	var header = {unknown:[]};
-	
 	var f = new PlanFile(filename);
+	
+	
+	
+	// Header einlesen
+	
+	var header = {unknown:[]};
 	
 	header.size = f.readInteger(2);
 	header.version = f.readInteger(2) + '.' + f.readInteger(2);
@@ -101,34 +105,75 @@ function decodePlanB(filename, outputFile) {
 	
 	header.description = f.readString(header.size - f.pos);
 	
+	
+	
+	// Daten einlesen
+	
 	var
-		data1 = [],
-		data2 = [];
+		list1 = [],
+		list2 = [],
+		list3 = [];
 		
 	for (var i = 0; i < header.listLength1; i++) {
-		data1[i] = [];
-		data1[i][0] = f.readInteger(2);
-		data1[i][1] = f.readInteger(2);
-		data1[i][2] = f.readInteger(2);
-		data1[i][3] = f.readInteger(4);
-		data1[i][4] = f.readInteger(4);
+		list1[i] = [];
+		list1[i][0] = f.readInteger(2);
+		list1[i][1] = f.readInteger(2);
+		list1[i][2] = f.readInteger(2);
+		list1[i][3] = f.readInteger(4);
+		list1[i][4] = f.readInteger(4);
+	}
+	
+	var lastIndex = -1;
+	for (var i = 0; i < header.listLength2; i++) {
+		list2[i] = [];
+		list2[i][0] = f.readInteger(4);
+		list2[i][1] = f.readInteger(4);
+	}
+	
+	var pos0 = f.pos;
+	for (var i = 0; i < header.listLength2; i++) {
+		list3[f.pos-pos0] = f.readNullString();
 	}
 	
 	for (var i = 0; i < header.listLength2; i++) {
-		data2[i] = [];
-		data2[i][0] = f.readInteger(4);
-		data2[i][1] = f.readInteger(4);
-	}
-	
-	for (var i = 0; i < header.listLength2; i++) {
-		data2[i][2] = f.readNullString();
+		list2[i][2] = list3[list2[i][1]];
 	}
 	
 	header.bytesLeft = f.check(outputFile);
 	
+	
+	
+	// Datenstruktur erzeugen
+	
+	var
+		data = [];
+		
+	for (var i = 0; i < header.listLength1; i++) {
+		data[i] = {
+			id: i,
+			unknown1: list1[i][0],
+			unknown2: list1[i][1],
+			unknown3: list1[i][2],
+			IBNR: list1[i][3],
+			name: list2[list1[i][4]][2],
+			synonyms: []
+		}
+		list1[i][5] = list2[list1[i][4]][2];
+	}
+	for (var i = 0; i < header.listLength2; i++) {
+		if (list1[list2[i][0]][4] != i) {
+			data[list2[i][0]].synonyms.push(list2[i][2]);
+		}
+	}
+	
+	
+	
+	// Alles exportieren
+	
 	exportHeader(outputFile, header);
-	exportTSV(outputFile, '1', data1);
-	exportTSV(outputFile, '2', data2);
+	exportTSV(outputFile, '1', list1);
+	exportTSV(outputFile, '2', list2);
+	exportJSON(outputFile, 'data', data);
 }
 
 function decodePlanBETR(filename, outputFile) {
