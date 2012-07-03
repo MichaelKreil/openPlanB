130e468c9586f623a6ad526fdc7bdc976fd82b88
Micho <github@michael-kreil.de>
2012-07-03 18:07:40 +0200

    planline

--- node.js/modules/plan_utils.js
+++ node.js/modules/plan_utils.js
@@ -61,6 +61,7 @@ function decodeFile(file, outputFolder) {
 		case 'plankant': decodePlanKANT( file.fullname, outputFile); break;
 		case 'plankgeo': decodePlanKGEO( file.fullname, outputFile); break;
 		case 'planlauf': decodePlanLAUF( file.fullname, outputFile); break;
+		case 'planline': decodePlanLINE( file.fullname, outputFile); break;
 		case 'planng':   decodePlanNG(   file.fullname, outputFile); break;
 		case 'planu':    decodePlanU(    file.fullname, outputFile); break;
 		case 'planvw':   decodePlanVW(   file.fullname, outputFile); break;
@@ -607,6 +608,49 @@ function decodePlanLAUF(filename, outputFile) {
 	
 	data1.pop();
 
+	header.bytesLeft = f.check(outputFile);
+	
+	exportHeader(outputFile, header);
+	exportTSV(outputFile, '1', data1);
+}
+
+function decodePlanLINE(filename, outputFile) {
+	var header = {unknown:[]};
+	
+	var f = new PlanFile(filename);
+	
+	header.size = f.readInteger(2);
+	
+	header.listLength1 = f.readInteger(2);
+	header.unknown.push(f.readInteger(2));
+	header.unknown.push(f.readInteger(2));
+	
+	header.version = f.readInteger(2) + '.' + f.readInteger(2);
+	header.creationDate = f.readTimestamp();
+	
+	header.unknown.push(f.readInteger(2));
+	header.unknown.push(f.readHexDump(4));
+	
+	header.description = f.readString(header.size - f.pos);
+	
+	var nameLength = 0;
+	switch (header.version) {
+		case '4.0': nameLength = 5; break;
+		case '4.1': nameLength = 8; break;
+		default:
+			console.error('ERROR: Unbekannte Version');
+	}
+	
+	var data1 = [];
+	
+	for (var i = 0; i < header.listLength1; i++) {
+		data1[i] = [];
+		data1[i][0] = f.readInteger(2);
+		data1[i][1] = f.readInteger(2);
+		data1[i][2] = f.readString(nameLength).replace(/\x00/, '');
+	}
+	
+	
 	header.bytesLeft = f.check(outputFile);
 	
 	exportHeader(outputFile, header);
