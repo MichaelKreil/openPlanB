8c3baa41f9fd31cbaa56350f891df8733e1f67ad
Micho <github@michael-kreil.de>
2012-07-03 22:51:15 +0200

    planuk

--- node.js/modules/plan_utils.js
+++ node.js/modules/plan_utils.js
@@ -64,6 +64,7 @@ function decodeFile(file, outputFolder) {
 		case 'planline': decodePlanLINE( file.fullname, outputFile); break;
 		case 'planng':   decodePlanNG(   file.fullname, outputFile); break;
 		case 'planu':    decodePlanU(    file.fullname, outputFile); break;
+		case 'planuk':   decodePlanUK(   file.fullname, outputFile); break;
 		case 'planvw':   decodePlanVW(   file.fullname, outputFile); break;
 		case 'planw':    decodePlanW(    file.fullname, outputFile); break;
 		case 'planzug':  decodePlanZUG(  file.fullname, outputFile); break;
@@ -796,6 +797,63 @@ function decodePlanU(filename, outputFile) {
 	exportHeader(outputFile, header);
 	exportTSV(outputFile, '1', data1);
 	exportTSV(outputFile, '2', data2);
+}
+
+function decodePlanUK(filename, outputFile) {
+	var header = {unknown:[]};
+	
+	var f = new PlanFile(filename);
+	
+	header.size = f.readInteger(2);
+	header.version = f.readInteger(2) + '.' + f.readInteger(2);
+	header.creationDate = f.readTimestamp();
+	
+	header.listLength1 = f.readInteger(2);
+	header.listLength4 = f.readInteger(4);
+	header.listLength2 = f.readInteger(4);
+	header.listLength3 = f.readInteger(4);
+	header.unknown.push(f.readInteger(4));
+	header.unknown.push(f.readInteger(4));
+	
+	header.unknown.push(f.readHexDump(4));
+	header.unknown.push(f.readInteger(4));
+	
+	header.description = f.readString(header.size - f.pos);
+	
+	// Schau mal, wie viele Bytes noch kommen und schätze dann mal die Größe von list2-Block3
+	// Irgendwie ergibt sich das nicht aus der Versionsnummer m(
+	
+	switch (f.length-f.pos) {
+		case header.listLength1*4 + header.listLength2*6 + header.listLength3*2 + header.listLength4*10:
+			header.list2Block3Size = 2;
+		break;
+		case header.listLength1*4 + header.listLength2*8 + header.listLength3*2 + header.listLength4*10:
+			header.list2Block3Size = 4;
+		break;
+		default:
+			console.error('ERROR: Byte-Länge geht irgendwie nicht auf.')
+	}
+	
+	var
+		data1 = [],
+		data2 = [],
+		data3 = [],
+		data4 = [];
+		
+	for (var i = 0; i < header.listLength1; i++) data1[i] = [f.readInteger(2), f.readInteger(2)];
+	
+	for (var i = 0; i < header.listLength2; i++) data2[i] = [f.readInteger(2), f.readInteger(2), f.readInteger(header.list2Block3Size)];
+	
+	for (var i = 0; i < header.listLength3; i++) data3[i] = f.readInteger(-2);
+	
+	for (var i = 0; i < header.listLength4; i++) data4[i] = [f.readInteger(-4), f.readInteger(2), f.readInteger(2), f.readInteger(2)];
+
+	header.bytesLeft = f.check(outputFile);
+	exportHeader(outputFile, header);
+	exportTSV(outputFile, '1', data1);
+	exportTSV(outputFile, '2', data2);
+	exportTSV(outputFile, '3', data3);
+	exportTSV(outputFile, '4', data4);
 }
 
 function decodePlanVW(filename, outputFile) {
