f8bad0cbc87aa2edacd0fc3fc5c4e19a9d3b578c
Thomas Rehn <thomas@carmen76.de>
2012-07-05 23:27:12 +0200

    GAT dekodieren

--- node.js/modules/plan_utils.js
+++ node.js/modules/plan_utils.js
@@ -57,6 +57,7 @@ function decodeFile(file, outputFolder) {
 		case 'planbetr': decodePlanBETR( file.fullname, outputFile); break;
 		case 'planbz':   decodePlanBZ(   file.fullname, outputFile); break;
 		case 'plancon':  decodePlanCON(  file.fullname, outputFile); break;
+		case 'plangat':  decodePlanGAT(  file.fullname, outputFile); break;
 		case 'plangls':  decodePlanGLS(  file.fullname, outputFile); break;
 		case 'planitxt': decodePlanITXT( file.fullname, outputFile); break;
 		case 'plankant': decodePlanKANT( file.fullname, outputFile); break;
@@ -498,6 +499,61 @@ function decodePlanGLS(filename, outputFile) {
 	exportTSV(outputFile, '2', data2);
 	exportTSV(outputFile, '3', data3);
 	exportTSV(outputFile, '4', data4);
+}
+
+function decodePlanGAT(filename, outputFile) {
+	var header = {unknown:[]};
+
+	var f = new PlanFile(filename);
+
+	header.size = f.readInteger(2);
+	header.version = f.readInteger(2) + '.' + f.readInteger(2);
+	header.creationDate = f.readTimestamp();
+
+	header.listLength1 = f.readInteger(2);
+	header.unknown.push(f.readInteger(4));
+	header.listLength2 = f.readInteger(2);
+	header.listLength3 = f.readInteger(4);
+	header.unknown.push(f.readInteger(4));
+
+	f.checkBytes('00 00 00 00');
+
+	header.unknown.push(f.readInteger(4));
+	header.unknown.push(f.readInteger(2));
+
+	header.description = f.readString(header.size - f.pos);
+
+	var
+		data1 = [],
+		data2 = [],
+		data3 = [];
+
+	for (var i = 0; i < header.listLength1; i++) {
+		data1[i] = [];
+		data1[i][0] = f.readString(4);
+		data1[i][1] = f.readInteger(2);
+		data1[i][2] = f.readString(8);
+		for (var j = 0; j < 10; ++j) {
+			data1[i][3 + j] = f.readInteger(2);
+		}
+	}
+
+	for (var i = 0; i < header.listLength2; i++) {
+		data2[i] = [];
+		data2[i][0] = f.readHexDump(614);
+	}
+
+	for (var i = 0; i < header.listLength3; i++) {
+		data3[i] = [];
+		data3[i][0] = f.readNullString();
+	}
+
+	header.bytesLeft = f.check(outputFile);
+
+	exportHeader(outputFile, header);
+	exportTSV(outputFile, '1', data1);
+	exportTSV(outputFile, '2', data2);
+	exportTSV(outputFile, '3', data3);
 }
 
 function decodePlanITXT(filename, outputFile) {
