1703b5a36f840a193971777ce417ae4fca4b5ef3
Micho <github@michael-kreil.de>
2012-07-08 20:29:53 +0200

    planMETA

--- node.js/modules/plan_utils.js
+++ node.js/modules/plan_utils.js
@@ -65,6 +65,7 @@ function decodeFile(file, outputFolder) {
 		case 'plankgeo': decodePlanKGEO( file.fullname, outputFile); break;
 		case 'planlauf': decodePlanLAUF( file.fullname, outputFile); break;
 		case 'planline': decodePlanLINE( file.fullname, outputFile); break;
+		case 'planmeta': decodePlanMETA( file.fullname, outputFile); break;
 		case 'planng':   decodePlanNG(   file.fullname, outputFile); break;
 		case 'planu':    decodePlanU(    file.fullname, outputFile); break;
 		case 'planuk':   decodePlanUK(   file.fullname, outputFile); break;
@@ -874,6 +875,55 @@ function decodePlanLINE(filename, outputFile) {
 	
 	exportHeader(outputFile, header);
 	exportTSV(outputFile, '1', data1);
+}
+
+function decodePlanMETA(filename, outputFile) {
+	var header = {unknown:[]};
+	
+	var f = new PlanFile(filename);
+	
+	header.size = f.readInteger(2);
+	header.version = f.readInteger(2) + '.' + f.readInteger(2);
+
+	header.unknown.push(f.readHexDump(4));
+	header.listLength2 = f.readInteger(4);
+	header.listLength3 = f.readInteger(4);
+	header.listLength1 = f.readInteger(4);
+	header.unknown.push(f.readHexDump(8));
+	header.unknown.push(f.readInteger(2));
+	header.unknown.push(f.readHexDump(4));
+	
+	header.description = f.readString(header.size - f.pos);
+	
+	var list1 = [];
+	for (var i = 0; i < header.listLength1; i++) {
+		list1[i] = [
+			f.readInteger(4),
+			f.readInteger(4),
+			f.readHexDump(8)
+		]
+	}
+	exportTSV(outputFile, '1', list1);
+	
+	var list2 = [];
+	for (var i = 0; i < header.listLength2; i++) {
+		list2[i] = f.readInteger(-4)
+	}
+	exportTSV(outputFile, '2', list2);
+	
+	var list3 = [];
+	for (var i = 0; i < header.listLength3; i++) {
+		list3[i] = [
+			f.readInteger(2),
+			f.readHexDump(1),
+			f.readHexDump(1),
+		]
+	}
+	exportTSV(outputFile, '3', list3);
+	
+	header.bytesLeft = f.check(outputFile);
+	
+	exportHeader(outputFile, header);
 }
 
 function decodePlanNG(filename, outputFile) {
