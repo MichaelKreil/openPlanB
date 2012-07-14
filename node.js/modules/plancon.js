d3eafaf9912b8df11b189905d0538cf2943d1c70
Micho <github@michael-kreil.de>
2012-07-14 23:57:09 +0200

    finishing planCON

--- node.js/modules/plancon.js
+++ node.js/modules/plancon.js
@@ -1,21 +1,25 @@
 var planUtils = require('./plan_utils.js');
 
 // Noch nicht fertig
-function decodePlanCON(filename, outputFile) {
+exports.decodePlan = function (filename, outputFile) {
 	var header = {unknown:[]};
 	
 	var f = new planUtils.PlanFile(filename);
+	
+	
+	
+	// reading Header
 	
 	header.size = f.readInteger(2);
 	header.version = f.readInteger(2) + '.' + f.readInteger(2);
 	header.creationDate = f.readTimestamp();
 	
-	header.unknown.push(f.readInteger(4));
+	header.listLength2 = f.readInteger(4);
 	header.listLength1 = f.readInteger(4);
-	header.unknown.push(f.readInteger(4));
+	header.listLength5 = f.readInteger(4);
 	header.unknown.push(f.readInteger(2));
-	header.unknown.push(f.readInteger(4));
-	header.unknown.push(f.readInteger(4));
+	header.listLength4 = f.readInteger(4);
+	header.listLength3 = f.readInteger(4);
 	header.unknown.push(f.readInteger(2));
 	header.unknown.push(f.readInteger(2));
 	header.unknown.push(f.readInteger(2));
@@ -25,35 +29,74 @@ function decodePlanCON(filename, outputFile) {
 	
 	header.description = f.readString(header.size - f.pos);
 	
-	var
-		data1 = [],
-		data2 = [];
 	
 	
+	// converting List 1
+	
+	var list1 = [];
 	for (var i = 0; i < header.listLength1; i++) {
-		data1[i] = [];
-		data1[i][0] = f.readInteger(4);
-		data1[i][1] = f.readHexDump(1);
-		data1[i][2] = f.readHexDump(1);
-		data1[i][3] = f.readInteger(4);
+		list1.push([
+			f.readInteger(4),
+			f.readHexDump(1),
+			f.readHexDump(1),
+			f.readInteger(4)
+		]);
 	}
+	planUtils.exportTSV(outputFile, '1', list1);
 	
-	/*
-	var i = 0;
-	var v;
-	do {
-		data2[i] = []; 
-		data2[i][0] = (v = f.readInteger(-4));
-		data2[i][1] = f.readInteger(-4);
-		console.log(v);
-		i++;
-	} while (v >= 0);
-	*/
+	
+	
+	// converting List 2
+	
+	var list2 = [];
+	for (var i = 0; i < header.listLength2; i++) {
+		list2.push(f.readInteger(4));
+	}
+	planUtils.exportTSV(outputFile, '2', list2);
+	
+	
+	
+	// converting List 3
+	
+	var list3 = [];
+	for (var i = 0; i < header.listLength3; i++) {
+		list3.push(f.readInteger(4));
+	}
+	planUtils.exportTSV(outputFile, '3', list3);
+	
+	
+	
+	// converting List 4
+	
+	var list4 = [];
+	if (header.listLength4 > 0) {
+		for (var i = 0; i < header.listLength4+1; i++) {
+			list4.push([
+				f.readInteger(-4),
+				f.readInteger(4)
+			]);
+		}
+	}
+	planUtils.exportTSV(outputFile, '4', list4);
+	
+	
+	
+	// converting List 5
+	
+	var list5 = [];
+	for (var i = 0; i < header.listLength5; i++) {
+		list5.push([
+			f.readInteger(4),
+			f.readInteger(2)
+		]);
+	}
+	planUtils.exportTSV(outputFile, '5', list5);
+	
+	
+	
+	// and we are finished
+	
 	header.bytesLeft = f.check(outputFile);
 	
 	planUtils.exportHeader(outputFile, header);
-	planUtils.exportTSV(outputFile, '1', data1);
-	planUtils.exportTSV(outputFile, '2', data2);
 }
-
-exports.decodePlan = decodePlanCON;
