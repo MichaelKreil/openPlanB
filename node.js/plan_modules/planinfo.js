0accb697acadd17c86bed35ccb32f547a90dd570
Micho <github@michael-kreil.de>
2012-07-15 19:38:21 +0200

    added 2 lists in planINFO

--- node.js/plan_modules/planinfo.js
+++ node.js/plan_modules/planinfo.js
@@ -8,21 +8,33 @@ exports.decodePlan = function(filename, outputFile) {
 	header.size = f.readInteger(2);
 	header.version = f.readInteger(2) + '.' + f.readInteger(2);
 	header.creationDate = f.readTimestamp();
-	/*
-	header.listLength2 = f.readInteger(4);
-	
-	header.unknown.push(f.readInteger(4));
-	header.listLength1 = f.readInteger(4);
-	header.unknown.push(f.readHexDump(4));
-	header.unknown.push(f.readHexDump(4));
-	*/
 	
 	header.unknown.push(f.readInteger(2));
 	header.unknown.push(f.readInteger(4));
-	header.unknown.push(f.readInteger(4));
+	header.listLength1 = f.readInteger(4);
 	header.unknown.push(f.readHexDump(4));
 
 	header.description = f.readString(header.size - f.pos);
+	
+	var list1 = [];
+	for (var i = 0; i < header.listLength1; i++) {
+		list1.push([
+			f.readInteger(4),
+			f.readInteger(4),
+			f.readInteger(2)
+		]);
+	}
+	planUtils.exportTSV(outputFile, '1', list1);
+	
+	var list2 = [];
+	var p0 = f.pos;
+	while (f.pos < f.length) {
+		list2.push([
+			f.pos - p0,
+			f.readNullString()
+		]);
+	}
+	planUtils.exportTSV(outputFile, '2', list2);
 	
 	header.bytesLeft = f.check(outputFile);
 	
