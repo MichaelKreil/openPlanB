4fdc6ba6a28672b309c569208b2820f752c383ad
Micho <github@michael-kreil.de>
2012-07-15 18:32:21 +0200

    finished planITXT

--- node.js/plan_modules/planitxt.js
+++ node.js/plan_modules/planitxt.js
@@ -17,8 +17,8 @@ function decodePlanITXT(filename, outputFile) {
 	header.unknown.push(f.readInteger(4));
 	header.listLength2 = f.readInteger(4);
 	header.listLength1 = f.readInteger(4);
-	header.unknown.push(f.readInteger(4));
-	header.unknown.push(f.readInteger(4));
+	header.listLength3 = f.readInteger(4);
+	header.listLength4 = f.readInteger(4);
 	header.unknown.push(f.readInteger(4));
 	header.unknown.push(f.readInteger(4));
 	
@@ -32,32 +32,70 @@ function decodePlanITXT(filename, outputFile) {
 	
 	header.description = f.readString(header.size - f.pos);
 	
-	var
-		data1 = [],
-		data2 = [];
-		
-	for (var i = 0; i < header.listLength1+1; i++) {
-		data1[i] = [];
-		data1[i][0] = f.readInteger(-4);
-		data1[i][1] = f.readInteger(-2);
-		data1[i][2] = f.readInteger(4);
-	}
 	
-	for (var i = 0; i < header.listLength2; i++) {
-		data2[i] = [];
-		data2[i][0] = f.readHexDump(4);
-		data2[i][1] = f.readHexDump(1);
-		data2[i][2] = f.readHexDump(1);
-		data2[i][3] = f.readHexDump(2);
-		data2[i][4] = f.readHexDump(2);
+	
+	var list1 = [];
+	for (var i = 0; i < header.listLength1+1; i++) {
+		list1[i] = [
+			f.readInteger(-4),
+			f.readInteger(-2),
+			f.readInteger(4)
+		];
 	}
+	planUtils.exportTSV(outputFile, '1', list1);
+	
+	
+	
+	var list2 = [];
+	for (var i = 0; i < header.listLength2; i++) {
+		list2[i] = [
+			f.readHexDump(4),
+			f.readHexDump(1),
+			f.readHexDump(1),
+			f.readHexDump(2),
+			f.readHexDump(2)
+		];
+	}
+	planUtils.exportTSV(outputFile, '2', list2);
+	
+	
+	
+	var list3 = [];
+	for (var i = 0; i < header.listLength3; i++) {
+		list3[i] = [
+			f.readInteger(4),
+			f.readInteger(4)
+		];
+	}
+	planUtils.exportTSV(outputFile, '3', list3);
+	
+	
+	
+	var list4 = [];
+	for (var i = 0; i < header.listLength4; i++) {
+		list4[i] = [
+			f.readInteger(1),
+			f.readInteger(1),
+			f.readInteger(1),
+			f.readInteger(1),
+			f.readInteger(4)
+		];
+	}
+	planUtils.exportTSV(outputFile, '4', list4);
+	
+	
+	
+	var list5 = [];
+	var p0 = f.pos;
+	while (f.pos < f.length) {
+		list5.push([f.pos-p0, f.readNullString()]);
+	}
+	planUtils.exportTSV(outputFile, '5', list5);
 	
 	
 	header.bytesLeft = f.check(outputFile);
 	
 	planUtils.exportHeader(outputFile, header);
-	planUtils.exportTSV(outputFile, '1', data1);
-	planUtils.exportTSV(outputFile, '2', data2);
 }
 
 exports.decodePlan = decodePlanITXT;
